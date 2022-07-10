2087c35b0fad17b0242de96305e5d28df42fdd01
KaRaKurT1 <lfyztdcttyru@bk.ru>
2022-07-11 04:17:50 +0500

    init commit

---
+++ script.js
@@ -0,0 +1,26 @@
+$(window).on("load", updateView())
+setInterval(updateView, 0)
+const slides = document.querySelectorAll('.slide')
+
+
+for (const slide of slides){
+    slide.addEventListener('click', () => {
+        clearActive()
+        slide.classList.add('active')
+        slide.classList.add('not-active')
+        slide.innerHTML += "<button type='button'>X</button>"
+    }
+    )
+} 
+
+function updateView(){
+	
+}
+
+function clearActive() {
+    slides.forEach( (slide) => {
+        slide.classList.remove('active')
+        slide.innerHTML = ""
+    })
+}
+
