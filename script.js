84161b43652c40d7fa43a6008f73dbb4619e8aa6
KaRaKurT1 <lfyztdcttyru@bk.ru>
2022-07-11 06:54:59 +0500

    доработка дизайна

--- script.js
+++ script.js
@@ -2,24 +2,32 @@ $(window).on("load", updateView())
 setInterval(updateView, 0)
 const slides = document.querySelectorAll('.slide')
 
-
 for (const slide of slides){
     slide.addEventListener('click', () => {
         clearActive()
         slide.classList.add('active')
-        slide.innerHTML += "<button type='button'>X</button>"
+        slide.innerHTML += "<button type='submit' id='submit'>X</button>"
+        const slidess = document.querySelectorAll('.slide')
+        for (const slide of slides){
+            slide.classList.add('not-active')
+        }
+        slide.classList.remove('not-active')
     }
     )
 } 
+
+function clearActive() {
+    slides.forEach( (slide) => {
+        slide.classList.remove('active')
+        slide.classList.remove('not-active')
+        slide.innerHTML = ""
+    })
+}
+
 
 function updateView(){
 	
 }
 
-function clearActive() {
-    slides.forEach( (slide) => {
-        slide.classList.remove('active')
-        slide.innerHTML = ""
-    })
-}
+
 
